769c5a23279ebcfff7b4fcbf5bf1e8956f525a76
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-07-05 01:13:32 +0530

    Test passed : Refactored code for better readability

--- src/main/java/com/incubyte/stringcalculator/SpringCalculator.java
+++ src/main/java/com/incubyte/stringcalculator/SpringCalculator.java
@@ -6,12 +6,6 @@ import org.springframework.stereotype.Component;
 public class SpringCalculator {
 
     public int add(String numbers){
-        // Handle empty or null input
-        if (numbers == null || numbers.isEmpty()) {
-            return 0;
-        }
-
-        // Placeholder return to handle non-empty cases later
-        return -1;
+        return (numbers == null || numbers.isEmpty()) ? 0 : -1;
     }
 }
